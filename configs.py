dd97e3c36126071c475bc32841297c24893cb2be
Yuvihy <119108846+Yuvihy@users.noreply.github.com>
2024-04-03 19:35:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "9b616a463537134e13a0da0ab7c96095d9f46ca8")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "1252715736"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://yashjadaun974:YChtg9WYjLJW3ujr@cluster0.x1qrmu9.mongodb.net/")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001973921006")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001983877514")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001973921006"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
